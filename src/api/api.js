92983d3dea1553d075e479a9d3d9067bc9631a00
Priya Deshmukh <priyadeshmukh15.pd@gmail.com>
2024-11-19 09:25:57 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export async function projectData(){
     const project = await axios.get(
-        "http://localhost:8080/api/project/details");
-        // "https://todo-backend1-xuw8.onrender.com/api/project/details");
+        // "http://localhost:8080/api/project/details");
+        "https://todo-backend1-xuw8.onrender.com/api/project/details");
     return project
 }
 
